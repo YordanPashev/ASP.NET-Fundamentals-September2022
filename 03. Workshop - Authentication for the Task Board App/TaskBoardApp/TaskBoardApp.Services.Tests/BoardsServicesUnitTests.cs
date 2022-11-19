a43001164ea576e3a74cc92517d89c43be6a7e40
Yordan Pasheff <yordan.pasheff@gmail.com>
2022-11-20 00:09:42 +0200

    Adding One more test for BoardsServices

--- 03. Workshop - Authentication for the Task Board App/TaskBoardApp/TaskBoardApp.Services.Tests/BoardsServicesUnitTests.cs
+++ 03. Workshop - Authentication for the Task Board App/TaskBoardApp/TaskBoardApp.Services.Tests/BoardsServicesUnitTests.cs
@@ -1,11 +1,13 @@
 namespace TaskBoardApp.Services.Tests
 {
     using System;
+    using System.Collections.Generic;
 
     using Microsoft.EntityFrameworkCore;
     using NUnit.Framework;
 
-    using TaskBoardApp.Data;
+    using Data;
+    using Models;
 
     [TestFixture]
     public class BoardsServicesUnitTests
@@ -17,14 +19,46 @@ namespace TaskBoardApp.Services.Tests
                     .UseInMemoryDatabase(Guid.NewGuid().ToString())
                     .Options;
             var dbContext = new TaskBoardDbContext(options, true);
-
             BoardsService boardsService = new BoardsService(dbContext);
+
             string boardName = ("TODO");
             int expectedBoardCount = 1;
 
             await boardsService.CreateNewBoardAsync(boardName);
             int realBoardCount = await dbContext.Boards.CountAsync();
-            Assert.That(realBoardCount == expectedBoardCount, "The count of all boards must be 1."); 
+
+            Assert.That(realBoardCount == expectedBoardCount, $"The count of all boards in db must be {expectedBoardCount}."); 
+        }
+
+        [Test]
+        public async System.Threading.Tasks.Task Test_GetAllBoardsAsync_Must_Return_True()
+        {
+            var options = new DbContextOptionsBuilder<TaskBoardDbContext>()
+                    .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                    .Options;
+            var dbContext = new TaskBoardDbContext(options, true);
+            BoardsService boardsService = new BoardsService(dbContext);
+
+            // The boards will be ordered alphabetically therefore the names must in same order.
+            string boardNameOne = "In Progress";
+            string boardNameTwo = "Open";
+            string boardNameThree = "TODO";
+            int expectedBoardCount = 3;
+
+            await boardsService.CreateNewBoardAsync(boardNameOne);
+            await boardsService.CreateNewBoardAsync(boardNameTwo);
+            await boardsService.CreateNewBoardAsync(boardNameThree);
+
+            // The boards will be ordered alphabetically
+            List<BoardViewModel> boardsResult =  await boardsService.GetAllBoardsAsync();
+
+            int firstBoardIndex = 0;
+            int secondBoardIndex = 1;
+            int thirdBoardIndex = 2;
+
+            Assert.That(boardsResult.Count == expectedBoardCount && boardsResult[firstBoardIndex].Name == boardNameOne &&
+                        boardsResult[secondBoardIndex].Name == boardNameTwo && boardsResult[thirdBoardIndex].Name == boardNameThree, 
+                        $"The result (count of all boards in db) be {expectedBoardCount} and the names of boards must be identical.");
         }
     }
 }
